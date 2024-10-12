502f85272561525a59977ee35c0f94a084c68cbb
KomendaKacper <komenda@student.agh.edu.pl>
2024-10-12 18:11:06 +0200

    Update

---
+++ restful-web-services/src/main/java/com/example/restful_web_services/user/User.java
@@ -0,0 +1,47 @@
+package com.example.restful_web_services.user;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
+
+import java.time.LocalDate;
+
+public class User {
+    private Integer id;
+    @Size(min=2, message = "Name should have atleast 2 characters")
+    @JsonProperty("user_name")
+    private String name;
+
+    @Past(message = "Birth Date should be in the past")
+    private LocalDate birthDate;
+
+    public User(Integer id, String name, LocalDate birthDate) {
+        this.id = id;
+        this.name = name;
+        this.birthDate = birthDate;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthday) {
+        this.birthDate = birthday;
+    }
+}
